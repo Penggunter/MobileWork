fcbbe9ecf6af92e27c0c4844a620189532fa8aef
shevi <shev.igor2002@yandex.ru>
2023-02-13 11:18:06 +0300

    Toast Commit

--- app/src/main/java/com/example/mobilework/MainActivity.java
+++ app/src/main/java/com/example/mobilework/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.mobilework;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Context context = getApplicationContext();
+        CharSequence text = "Hello toast!";
+        int durationTime = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, durationTime);
+        toast.show();
+
     }
     /* Hello world */
     /* Hello world again */
