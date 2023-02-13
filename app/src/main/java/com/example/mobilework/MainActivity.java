4212458f9dca6181828469973b9e4c24d52ffb94
shevi <shev.igor2002@yandex.ru>
2023-02-13 12:33:22 +0300

    Log Commit

--- app/src/main/java/com/example/mobilework/MainActivity.java
+++ app/src/main/java/com/example/mobilework/MainActivity.java
@@ -4,42 +4,50 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    //Context context = getApplicationContext();
-    //int durationTime = Toast.LENGTH_SHORT;
+    private static final String TAG = "MyApp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // -- Toast
         Context context = getApplicationContext();
         int durationTime = Toast.LENGTH_SHORT;
         CharSequence text = "Activity create";
 
         Toast toast = Toast.makeText(context, text, durationTime);
         toast.show();
+
+        // -- Log
+        Log.i(TAG, "The application was created by student Igor from IKBO-25-21");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
 
+        // -- Toast
         Context context = getApplicationContext();
         int durationTime = Toast.LENGTH_SHORT;
         CharSequence text = "Activity start";
 
         Toast toast = Toast.makeText(context, text, durationTime);
         toast.show();
+
+        // -- Log
+        Log.d(TAG, "Application start");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
+        // -- Toast
         Context context = getApplicationContext();
         int durationTime = Toast.LENGTH_SHORT;
         CharSequence text = "Activity resume";
@@ -47,36 +55,51 @@ public class MainActivity extends AppCompatActivity {
         Toast toast = Toast.makeText(context, text, durationTime);
         toast.show();
 
+        // -- Log
+        Log.v(TAG, "other information");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
 
+        // -- Toast
         Context context = getApplicationContext();
         int durationTime = Toast.LENGTH_SHORT;
         CharSequence text = "Activity pause";
 
         Toast toast = Toast.makeText(context, text, durationTime);
         toast.show();
+
+        // -- Log
+        try {
+            Log.w(TAG, "Possible exception");
+        }catch (Exception exception) {
+            Log.e(TAG,"Exception received", exception);
+        }
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
 
+        // -- Toast
         Context context = getApplicationContext();
         int durationTime = Toast.LENGTH_SHORT;
         CharSequence text = "Activity restart";
 
         Toast toast = Toast.makeText(context, text, durationTime);
         toast.show();
+
+        // -- Log
+        Log.d(TAG, "Application restart");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
 
+        // -- Toast
         Context context = getApplicationContext();
         int durationTime = Toast.LENGTH_SHORT;
         CharSequence text = "Activity stop";
@@ -84,18 +107,24 @@ public class MainActivity extends AppCompatActivity {
         Toast toast = Toast.makeText(context, text, durationTime);
         toast.show();
 
+        // -- Log
+        Log.d(TAG, "Application stop");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
 
+        // -- Toast
         Context context = getApplicationContext();
         int durationTime = Toast.LENGTH_SHORT;
         CharSequence text = "Activity destroy";
 
         Toast toast = Toast.makeText(context, text, durationTime);
         toast.show();
+
+        // -- Log
+        Log.d(TAG, "Application destroy");
     }
     /* Hello world */
     /* Hello world again */
